954937575328a40d22e0ba39873c2365a3ce0883
halityarimbatman <halityarimbatman@gmail.com>
2026-01-06 18:01:45 +0300

    Delete Tarih Yolcusu/game_data.py

--- Tarih Yolcusu/game_data.py
+++
@@ -1,6 +0,0 @@
-# Oyun İçeriği Veritabanı
-            artifacts = {
-                'Hitit Gunesi': {'image': 'graphics/artifacts/hitit_gunesi.png', 'desc': 'Hititlerin dini sembolü.'},
-                'Frig Basligi': {'image': 'graphics/artifacts/frig_basligi.png', 'desc': 'Kral Midas in taktigi unlu baslik.'}
-            }
-            
